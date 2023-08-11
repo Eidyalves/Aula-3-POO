09e03dedc64a53d162898458018b8fdbab68918e
2023102060051 <2023102060051@JIPA-LABIN03-11>
2023-08-10 20:27:49 -0400

    Adicionar arquivos de projeto.

---
+++ Aula 3 POO/Conta.cs
@@ -0,0 +1,43 @@
+﻿
+public class Conta
+{
+    public string nomeTitular;
+    public string agenciaTitular;
+    public string contaTitular;
+    public double saldoConta;
+
+    public Conta(string nomeTitular, string agenciaTitular, string contaTitular, double saldoConta)
+    {
+        this.nomeTitular = nomeTitular;
+        this.agenciaTitular = agenciaTitular;
+        this.contaTitular = contaTitular;
+        this.saldoConta = saldoConta;
+    }
+
+    public double Depositar(double valorDeposito)
+    {
+        if (valorDeposito > 0)
+        {
+            this.saldoConta += valorDeposito;
+            return saldoConta;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido");
+            return 0;
+        }
+    }
+    public double Sacar(double valorSaque)
+    {
+        if(valorSaque <= this.saldoConta && valorSaque > 0)
+        {
+            this.saldoConta -= valorSaque;
+            
+        }
+        else
+        {
+            Console.WriteLine("Saldo Insuficiente");
+        }
+        return this.saldoConta;
+    }
+}
---
+++ Aula 3 POO/Program.cs
@@ -0,0 +1,27 @@
+﻿List<Conta> contas = new List<Conta>();
+while (true)
+{
+    Console.WriteLine("Digite 1 para cadastrar uma conta e 2 para listar todos");
+
+    int controlador = Convert.ToInt32(Console.ReadLine());
+    if (controlador == 1)
+    {
+        Console.WriteLine("Informe o nome, agencia, conta e o saldo");
+        string titular = Console.ReadLine();
+        string agencia = Console.ReadLine();
+        string conta = Console.ReadLine();
+        double saldo = Convert.ToDouble(Console.ReadLine());
+        Conta c = new Conta(titular, agencia, conta, saldo);
+        contas.Add(c);
+    }else if (controlador == 2)
+    {
+        foreach (Conta conta in contas)
+        {
+            Console.WriteLine($"Titular: {conta.nomeTitular}, saldo: R$ {conta.saldoConta}");
+        }
+    }
+    else
+    {
+        break;
+    }
+}
